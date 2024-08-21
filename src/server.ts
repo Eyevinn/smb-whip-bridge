2145634f19448270b21c858d7906ae2cc8a2b5d7
Jonas Birmé <jonas.birme@eyevinn.se>
2024-08-21 18:11:29 +0200

    feat: support whep endpoint service

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { WhipEndpoint } from '@eyevinn/whip-endpoint';
+import { BroadcasterClient, WhipEndpoint } from '@eyevinn/whip-endpoint';
 
 if (!process.env.SMB_URL) {
   throw new Error('SMB_URL environment variable is required');
@@ -10,11 +10,18 @@ const server = new WhipEndpoint({
   port: PORT,
   enabledWrtcPlugins: ['sfu-broadcaster']
 });
-server.setOriginSfuUrl(
-  new URL('/conferences/', process.env.SMB_URL).toString()
-);
+const sfuUrl = new URL('/conferences/', process.env.SMB_URL);
+server.setOriginSfuUrl(sfuUrl.toString());
 if (process.env.SMB_API_KEY) {
   server.setSfuApiKey(process.env.SMB_API_KEY);
+}
+if (process.env.WHEP_ENDPOINT_URL) {
+  const whepChannelApiUrl = new URL('/api', process.env.WHEP_ENDPOINT_URL);
+  const whepClient = new BroadcasterClient(whepChannelApiUrl.toString());
+  server.registerBroadcasterClient({
+    client: whepClient,
+    sfuUrl: sfuUrl.toString()
+  });
 }
 
 server.listen();
